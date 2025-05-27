3f05c0a2cac46e28cf1e6d3a339e08c948d3908c
Zhupi222 <zhupi222@126.com>
2025-05-28 00:04:04 +0800

    (fix) Cannot apply i18n in SSR components

--- src/lib/blog/server.ts
+++ src/lib/blog/server.ts
@@ -1,10 +1,12 @@
 import { dbConnect } from '@/lib/db';
 import BlogModel from '@/models/Blog';
 import type { Blog } from './types';
+import { Locale } from '@/i18n/types';
 
-export async function getBlogBySlug(slug: string): Promise<Blog | null> {
+export async function getBlogBySlug(slug: string, locale?: Locale): Promise<Blog | null> {
   await dbConnect();
-  return (await BlogModel.findOne({ slug }).lean()) as Blog | null;
+  const query = locale ? { slug, language: locale } : { slug };
+  return (await BlogModel.findOne(query).lean()) as Blog | null;
 }
 
 export async function getAllBlogSlugs() {
